2d1327cfa1f49a94eec6ec8b664833181a81225b
Brooke Wolford <bwolford@umich.edu>
2018-09-24 15:19:26 -0400

    Adding multiselectOut function

--- R/lolliplot.R
+++ R/lolliplot.R
@@ -300,6 +300,7 @@ lolliplot_buildMain <- function(gene_data, length, mutation_observed,
                                 plot_sidechain=FALSE,layers=NULL,
                                 paletteA=NULL, paletteB=NULL)
 {
+  
   # build the various features of the lolliplot
   
   # Build gene base either using domain information
@@ -1111,3 +1112,45 @@ lolliplot_transcriptID2codingSeq <- function(transcriptID,
   
   return(as.list(result))
 }
+
+#' Choose output
+#' 
+#' Selector for choosing output for GenVisR functions
+#' 
+#' @name multi_selectOut
+#' @param data Data object to output
+#' @param plot Plot object to output
+#' @param out Character vector specifying the the object to output, one of
+#' "data", "grob", or "plot".
+#' @param draw Boolean specifying if the input to plot needs to be drawn
+#' @return One of the following, a list of dataframes containing data to be
+#' plotted, a grob object, or a plot.
+#' @importFrom ggplot2 ggplotGrob
+#' @importFrom grid grid.draw
+#' @noRd
+
+multi_selectOut <- function(data, plot, out="plot", draw="FALSE")
+{
+  # Decide what to output
+  if(toupper(out) == "DATA")
+  {
+    return(data)
+  } else if(toupper(out) == "PLOT" & isTRUE(draw)) {
+    return(grid::grid.draw(plot))
+  } else if(toupper(out) == "PLOT" & !isTRUE(draw)) {
+    return(plot)
+  } else if(toupper(out) == "GROB" & isTRUE(draw)) {
+    return(plot)
+  } else if(toupper(out) == "GROB" & !isTRUE(draw)) {
+    return(ggplot2::ggplotGrob(plot))
+  } else {
+    warning("Did not recognize input to out...")
+    if(isTRUE(draw))
+    {
+      return(grid::grid.draw(plot))
+    } else {
+      return(plot)
+    }
+  }
+}
+
